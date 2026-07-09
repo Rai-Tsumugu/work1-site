6e577e8141d1746735e31c7a5d62c41f76aa58f7
yuta ebina <youtan12230@gmail.com>
2026-07-09 16:27:10 +0900

    e

--- tabs.js
+++ tabs.js
@@ -1,8 +1,7 @@
 // 自分のタブを1行追加してください（最後の行の下に）
 // 例: { name: "おみくじ", file: "tabs/yamada.html", author: "山田" },
 const TABS = [
-  { name: "見本: あいさつ", file: "tabs/_sample.html", author: "先生" },
-  { name: "おみくじ", file: "tabs/omikuji.html", author: "先生" },
-  { name: "割り勘計算", file: "tabs/warikan.html", author: "先生" },
+  { name: "おみ9じ", file: "tabs/omikuji.html", author: "先生" },
+  { name: "学食人気投票", file: "tabs/index.html", author: "俺" },
   // ↓この下に追加
 ];
